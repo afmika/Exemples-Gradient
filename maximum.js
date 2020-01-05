3c155ad12c309f4e1c202ad61aaa3bb4e45b3e3f
afmika <afmichael73@gmail.com>
2020-01-05 13:50:59 +0300

    Version 4 +++ Organisation en dossier

--- maximum.js
+++ maximum.js
@@ -17,7 +17,7 @@
  let pas = 0.01;
  let x = 1;
  while(etapes > 0) {
-	// gradient negatif si on recherche le max
+	// gradient positif si on recherche le max
 	 x = x + pas * derive_de_f( x );
 	 etapes--;
 	 console.log(x);
